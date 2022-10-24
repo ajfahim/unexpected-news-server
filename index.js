2f4fa4f5ee616fd941f047abba23ed750e0e88e0
ajfahim <ajfahim52@gmail.com>
2022-10-24 18:22:05 +0600

    added news/id route

--- index.js
+++ index.js
@@ -16,10 +16,27 @@ app.get("/categories", (req, res) => {
     res.send(categories);
 })
 
+app.get("/category/:id", (req, res) => {
+    const id = req.params.id
+
+    if (id === "08") {
+        res.send(news);
+    }
+    else {
+        const newsCategory = news.filter(n => n.category_id === id)
+        res.send(newsCategory);
+    }
+
+})
+
+app.get("/news", (req, res) => {
+    res.send(news)
+})
+
 app.get("/news/:id", (req, res) => {
     const id = req.params.id
-    const newsCategory = news.filter(n => n._id === id)
-    res.send(newsCategory);
+    const singleNews = news.find(n => n._id === id)
+    res.send(singleNews);
 })
 
 app.listen(port, () => {
